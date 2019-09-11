2b95ed7cd663da771428212e8882f18e2d81855b
Dan Valinotti <dvalinotti@galaxe.com>
2019-09-11 13:24:03 -0400

    Added Dashboard and Reports tests & refined Drug Search tests

--- test/frontend.test.js
+++ test/frontend.test.js
@@ -3,9 +3,11 @@ const { describe, it, after, before } = require('mocha');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const globals = require('../globals.js');
 const drugs = require('../lib/drugs');
 const {By, until} = webdriver;
+const fs = require('fs');
 chai.use(chaiAsPromised);
 process.on('unhandledRejection', () => {});
 
@@ -18,19 +20,35 @@ let driver;
     try {
         describe ('Frontend Tests', async function() {
             this.timeout(15000);
+            let dl_dir;
+            // Build Chrome WebDriver
             before(() => {
+                dl_dir = "C:\\Users\\dvalinotti\\Desktop\\GalaxE_Workspace\\RxWaveTests\\downloads";
+                let options = new chrome.Options();
+                options.setUserPreferences({
+                    "plugins.plugins_list": [{"enabled":false,"name":"Chrome PDF Viewer"}],
+                    "download": {
+                        "prompt_for_download": false,
+                        "default_directory"  : dl_dir
+                    }
+                });
                 driver = new webdriver.Builder()
                     .forBrowser('chrome')
+                    .setChromeOptions(options)
                     .build();
-            })
+            });
 
+            // Login Tests suite
             describe ('Login tests', async function() {
+
+                // Tests if login page loads
                 it ('Test page load', async function() {
                     await driver.get(globals.SITE);
                     let title = await driver.getTitle();
                     chai.assert.equal(title, 'Inside Rx');
                 });
-    
+
+                // Test is login with admin creds is successful    
                 it ('Test login authentication', async function() {
                     let fields = await driver.findElements(By.css('.MuiInputBase-input.MuiOutlinedInput-input'));
                     await fields[0].sendKeys(globals.SITE_USERNAME);
@@ -44,105 +62,194 @@ let driver;
     
                     chai.assert.equal(url, globals.SITE + '/#/search');
                 });
-            })
+            });
+
+            // Dashboard page Test Suite
+            describe ('Dashboard Page Tests', async function() {
+                
+                // Test if page loads
+                it ('Test if Dashboard page loads', async function() {
+                    let tabs = await driver.findElements(By.css('button[role="tab"]'));
+                    await tabs[1].click();
+                    
+                    await driver.wait(async function() {
+                        try {
+                            await driver.findElement(By.css('span.programPrice>div>span')).isDisplayed();
+                            return true;
+                        } catch (e) {
+                            return false;
+                        }
+                    });
+
+                    let currentUrl = await driver.getCurrentUrl();
+                    chai.assert.isTrue(currentUrl === globals.SITE + "/#/viewDashboard");
+                });
+
+                // Test if prices in report are not $0.00
+                it ('Test if prices display', async function() {
+                    let dashPrices = await driver.findElements(By.css('span.programPrice>div>span'));
+                    let pass = true;
+                    for (const p of dashPrices) {
+                        let t = p.getText();
+                        if (t === "$0.00") {
+                            pass = false;
+                        }
+                    }
+
+                    chai.assert.isTrue(pass);
+                })
+            });
+
+            // Reports page Test Suite
+            describe('Reports Page Tests', async function() {
+
+                // Test if page loads
+                it ('Test if Reports page loads', async function() {
+                    let tabs = await driver.findElements(By.css('button[role="tab"]'));
+                    await tabs[2].click();
+                    
+                    await driver.wait(async function() {
+                        try {
+                            await driver.findElement(By.css('svg.pointer')).isDisplayed();
+                            return true;
+                        } catch (e) {
+                            return false;
+                        }
+                    });
+
+                    let currentUrl = await driver.getCurrentUrl();
+                    chai.assert.isTrue(currentUrl === globals.SITE + "/#/reports");
+                });
+
+                // Downloads latest report and tests if file exists
+                it ('Test if latest report successfully downloads', async function() {
+                    await driver.findElement(By.css('svg.pointer')).click();
+                    // Wait for file to download
+                    await sleep(3000);
+
+                    // Finds downloaded file - if found, deletes
+                    let found = false;
+                    try {
+                        await fs.promises.access(dl_dir + "\\poi-generated-file.xlsx");
+                        found = true;
+                        await fs.promises.unlink(dl_dir + "\\poi-generated-file.xlsx")
+                    } catch (error) {
+                        console.log("Downloaded report not found: ", error);
+                    }
+
+                    chai.assert.isTrue(found);
+                });
+            });
+
+            // Drug Search test suite
             describe ('Drug Search Tests', async function() {
+                // Loop through list of drugs in `drugs.js`
                 drugs.forEach((drug) => {
+
+                    // Test suite for specific drug
                     describe (`Test search for drug ${drug.Name} `, async function() {
                         let prices;
-                        let program_id = 0;
 
+                        // Instantiate prices array
                         before (() => {
                             prices = [];
                         }); 
 
+                        // See below. vvv
                         it ('Test if search is successful ', async function() {
-                            let price = undefined;
+                            let averagePrice = undefined;
+                            let currentPrice = undefined;
+                            let lmPrice = undefined;
                             try {
+                                let tabs = await driver.findElements(By.css('button[role="tab"]'));
+                                await tabs[0].click();
                                 await driver.findElement(By.id('downshift-simple-input')).sendKeys(drug.Name);
-                                await sleep(1250);
+                                await sleep(1250);    // Wait for search suggestions to populate
                                 await driver.findElement(By.id('downshift-simple-item-0')).click();
                                 await driver.findElement(By.id('myZipCode')).sendKeys(drug.ZipCode);
-                                await driver.findElement(By.css('.form-control[name="drugStrength"]')).click();
-        
-                                console.log("SEARCH HERE");
-        
+
+                                 // Finds correct dosage and selects
+                                await driver.findElement(By.css('.form-control[name="drugStrength"]')).click();                
                                 let dosages = await driver.findElements(By.css('.form-control[name="drugStrength"]>option'));
-        
-                                for (const dosage of dosages) {
+                                for (const dosage of dosages) {                            
                                     let text = await dosage.getText();
                                     if (text.includes(drug.Dosage)) {
                                         await dosage.click();
-                                        console.log("clicked d");
+                                        // console.log("clicked d");
                                     }
                                 }
-                                
+                                // Finds correct quantity and selects
                                 await driver.findElement(By.css('select[name="drugQuantity"]')).click();
                                 let quantities = await driver.findElements(By.css('select[name="drugQuantity"]>option'));
-        
-                                for (const quantity of quantities) {
+                                for (const quantity of quantities) {                        
                                     let text = await quantity.getText();
                                     if (text.includes(drug.Quantity)) {
                                         await quantity.click();
-                                        console.log("clicked q");
+                                        // console.log("clicked q");
                                     }
                                 }
                                 
+                                // Wait for search to finish before continuing
                                 await driver.findElement(By.css('.form-control.pointer')).click();
-                                await driver.wait(async function() {
+                                await driver.wait(async function() {                       
                                     try {
                                         await driver.findElement(By.className('MuiDialog-root')).isDisplayed();
                                         return false;
                                     } catch (e) {
-                                        console.log("Dialog not found");
                                         return true;
                                     }
                                 });
                                 
                                 try {
-                                    price = await driver.findElement(By.css('.overallPrice.col-sm>.headerhelp')).getText();
+                                    // Retrieves all header prices
+                                    let overallPrices = await driver.findElements(By.css('div.overallPrice>div.headerhelp'));
+                                    averagePrice = await overallPrices[0].getText();
+                                    currentPrice = await overallPrices[1].getText();
+                                    lmPrice = await overallPrices[2].getText();
+
+                                    // Save each program's price in prices array
                                     let priceElements = await driver.findElements(By.className('compPrice'));
-                                    let count = 0;
-                                    for (let i = 0; i < priceElements.length; i++) {
+                                    for (let i = 0; i < priceElements.length; i++) {            
                                         let text = await priceElements[i].getText();
-                                        if (text !== "N/A") {
-                                            prices.push(text);
-                                        }
+                                        prices.push(text);
                                     }
                                     await driver.findElement(By.css('.MuiButtonBase-root.MuiTab-root.MuiTab-textColorInherit.Mui-selected')).click();
+
+                                    // Clear text fields in the case of a failed search
                                     await driver.findElement(By.id('downshift-simple-input')).clear();
                                     await driver.findElement(By.id('myZipCode')).clear();
                                 } catch (e) {
-                                    price = undefined;
-                                    console.log("ERROR");
+                                    // Refresh if error occurs
                                     await driver.navigate().refresh();
                                 }
                             } catch (e) {
                                 console.log(e);
                             } finally {
-                                chai.assert.isTrue(price !== undefined);
+                                // Asserts that all header prices exist
+                                chai.assert.isTrue(
+                                    averagePrice !== "N/A" &&
+                                    currentPrice !== "N/A" &&
+                                    lmPrice !== "N/A"
+                                );
                             }
                         });
                         
-                        // it ('Check if price exists for SingleCare', function() {
-                        //     chai.assert.isTrue(prices[0] !== undefined);
-                        // });
-                        // it ('Check if price exists for MedImpact', function() {
-                        //     chai.assert.isTrue(prices[1] !== undefined);
-                        // });
-                        // it ('Check if price exists for WellRx', function() {
-                        //     chai.assert.isTrue(prices[2] !== undefined);
-                        // });
-                        // it ('Check if price exists for US Pharmacy Card', function() {
-                        //     chai.assert.isTrue(prices[3] !== undefined);
-                        // });
+                        // Test Blink Health price exists
                         it ('Check if price exists for Blink Health', function() {
-                            chai.assert.isTrue(prices[4] !== undefined);
+                            chai.assert.isTrue(prices[4] !== "N/A");
                         });
+                        
+                        // Test GoodRx price exists
                         it ('Check if price exists for GoodRx', function() {
-                            chai.assert.isTrue(prices[5] !== undefined);
+                            chai.assert.isTrue(prices[5] !== "N/A");
                         });
                     });
                 });
+            });
+
+            // Quit driver when finished testing
+            after(() => {
+                driver.quit();
             });
         });
     } catch(error) {
